656bd6613767478c236034cc3a46f2843a2d978c
anek05 <andreas.eklund94@gmail.com>
2022-01-13 00:00:48 +0100

    post image code

--- src/WeddingForm.js
+++ src/WeddingForm.js
@@ -2,30 +2,59 @@ import React from "react";
 import axios from "axios";
 
 class WeddingForm extends React.Component {
+  handleFileSelect(evt) {
+    var reader = new FileReader();
+    var file = (file = document.querySelector("input[name='file']").files[0]);
+    console.log(typeof file);
+    var fileName = file.name;
+    // reader.onerror = this.errorHandler;
+    reader.onload = function (e) {
+      var html =
+        '<input type="" id="data" value="' +
+        e.target.result.replace(/^.*,/, "") +
+        '" >';
+      html +=
+        '<input type="" id="mimetype" value="' +
+        e.target.result.match(/^.*(?=;)/)[0] +
+        '" >';
+      html += '<input type="" id="filename" value="' + fileName + '" >';
+      console.log("test");
+      document.getElementById("filedata").innerHTML = html;
+    };
+    reader.readAsDataURL(file);
+  }
+
   /********************** RSVP **********************/
   handleSubmit(e) {
     e.preventDefault();
-    var form = document.querySelector("form");
+    // var form = document.querySelector("form");
+    // var data = new URLSearchParams(new FormData(form)).toString();
 
     var nameInput = document.getElementById("name").value;
     var mailInput = document.getElementById("email").value;
-    var imagefile = document.querySelector("#file");
-    var imageid = document.getElementById("file");
+    // var imagefile = document.querySelector("#file");
     console.log("test");
-    console.log(imageid);
     var formData = new FormData();
     formData.append("name", nameInput);
     formData.append("email", mailInput);
-    formData.append("file", imagefile.files[0]);
+    // formData.append("file", imagefile.files[0]);
+
+    var data = document.querySelector("div[id='data']");
+    console.log(data);
+    var fileData = document.getElementById("data").value;
+    var mimetype = document.getElementById("mimetype").value;
+    var filename = document.getElementById("filename").value;
+
+    formData.append("data", fileData);
+    formData.append("mimetype", mimetype);
+    formData.append("filename", filename);
     for (var pair of formData.entries()) {
       console.log(pair[0] + ", " + pair[1]);
     }
-    var data = new URLSearchParams(new FormData(form)).toString();
-    // console.log(data);
 
     axios
       .post(
-        "https://script.google.com/macros/s/AKfycbyrYR1w0XNi2k2LPLMJOH94CUIqbZdzrsFCxehu_glRzm0pSoLuL1qlmF3N_3f5N4rCqQ/exec",
+        "https://script.google.com/macros/s/AKfycbwNoVkECUG46i4xXe5JRde8-YXS_ftqiCHFuvGGhGdOqrSFhdWFqN76Are-WZE6R7Bs0w/exec",
         formData,
         {
           headers: {
@@ -78,13 +107,15 @@ class WeddingForm extends React.Component {
               type="file"
               id="file"
               accept="image/png, image/jpeg"
-              name="thefile"
+              name="file"
               class=""
               placeholder="Bild"
               required
+              onChange={this.handleFileSelect}
             />
           </div>
         </div>
+        <div id="filedata"></div>
         <div class="row">
           <div class="col-md-12" id="alert-wrapper"></div>
         </div>
